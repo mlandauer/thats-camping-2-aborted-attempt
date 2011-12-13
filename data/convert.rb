e748c114a3f190e21ff53502ed7563e16133c986
Matthew Landauer <matthew@openaustralia.org>
2011-12-13 15:12:09 +1100

    Pretty print json output

--- data/convert.rb
+++ data/convert.rb
@@ -8,7 +8,9 @@ require 'json'
 
 def convert_plist_to_json(input, output)
   result = Plist::parse_xml(File.join(File.dirname(__FILE__), input))
-  File.open(File.join(File.dirname(__FILE__), output), 'w') { |f| f.write(result.to_json) }
+  File.open(File.join(File.dirname(__FILE__), output), 'w') do |f|
+    f.write(JSON.pretty_generate(result))
+  end
 end
 
 convert_plist_to_json('Campsites.plist', 'campsites.json')
