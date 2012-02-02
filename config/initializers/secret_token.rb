9947eeb7b9fa526f92b25723b80a96b8530767c6
Matthew Landauer <matthew@openaustralia.org>
2012-02-03 10:42:57 +1100

    rails new . (version 3.2)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThatsCamping2::Application.config.secret_token = '850480275bd83697ea8e4cdbd331c39855d282fac745258fd438843e1d5b169cad1023ee275b9b42d9a5e7b7975b5a177ee6a1b73ab248db4a0af29d7494d95f'
